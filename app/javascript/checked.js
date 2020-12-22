d44e2aa8f493de007c720b606ea1ac44253774d7
RyouheiMatsuyama <ryohei.0427@ezweb.ne.jp>
2020-12-22 15:45:56 +0900

    既読機能のフロントエンド実装2

--- app/javascript/checked.js
+++ app/javascript/checked.js
@@ -1,6 +1,7 @@
 function check() {
   const posts = document.querySelectorAll(".post");
   posts.forEach(function (post) {
+<<<<<<< Updated upstream
     if (post.getAttribute("date-load") != null) {
       return null;
     }
@@ -8,6 +9,14 @@ function check() {
     post.setAttribute("date-load", "true");
     post.addEventListener("click", () => {
       const postId = post.getAttribute("date-id");
+=======
+    if (post.getAttribute("data-load") != null) {
+      return null;
+    }
+    post.setAttribute("data-load", "true");
+    post.addEventListener("click", () => {
+      const postId = post.getAttribute("data-id");
+>>>>>>> Stashed changes
       const XHR = new XMLHttpRequest();
       XHR.open("GET", `/posts/${postId}`, true);
       XHR.responseType = "json";
@@ -15,6 +24,7 @@ function check() {
       XHR.onload = () => {
         if (XHR.status != 200) {
           alert(`Error ${XHR.status}: ${XHR.statusText}`);
+<<<<<<< Updated upstream
           return null;
         }
         const item = XHR.response.post;
@@ -22,6 +32,15 @@ function check() {
           post.setAttribute("date-check", "true");
         } else if (item.checked === false) {
           post.removeAttribute("date-check");
+=======
+          return null;          
+        }
+        const item = XHR.response.post;
+        if (item.checked === true) {
+          post.setAttribute("data-check", "true");
+        } else if (item.checked === false) {
+          post.removeAttribute("data-check");
+>>>>>>> Stashed changes
         }
       };
     });
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root to: 'posts#index'
   post 'posts', to: 'posts#create'
+<<<<<<< Updated upstream
   get 'posts', to: 'posts#checked'
+=======
+>>>>>>> Stashed changes
   get 'posts/:id', to: 'posts#checked'
 end
